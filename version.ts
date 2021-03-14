41ca9709cf84d8b2101cf1e2a40ade666fb55197
Philip Bordallo <git@philipbordallo.com>
2021-03-14 14:37:51 -0400

    Add version script

---
+++ version.ts
@@ -0,0 +1,17 @@
+import fs from 'fs';
+
+import pkg from './package.json';
+import data from './data.json';
+
+
+async function run() {
+  const newData = {
+    ...data,
+    version: pkg.version,
+  };
+
+  fs.promises.writeFile('./data.json', JSON.stringify(newData, null, 2));
+}
+
+run().catch(console.error);
+
